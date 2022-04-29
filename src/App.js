bbb32086ec1fc5efa5f1d5ba94fc9de3b338d6d4
momaymt11 <Thaswan.momay11@gmail.com>
2022-04-29 14:42:07 +0700

    update donate

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 
-const ADDRESS = "0xa9C0b1aF507c6A9585755e4acB462fD8BAbA85Ce";
+const ADDRESS = "0x10e1bc34ae1a5c3a70c0cb59e6457ceb0a4dd9e9";
 const ABI = [
   {
     inputs: [
@@ -38,6 +38,19 @@ const ABI = [
     type: "function",
   },
   {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+    ],
+    name: "doDonatetoDog",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
     inputs: [],
     name: "doDonation",
     outputs: [],
@@ -51,7 +64,64 @@ const ABI = [
   },
   {
     inputs: [],
-    name: "getDog",
+    name: "balance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "Dogs",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "city",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "note",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "img",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getDogs",
     outputs: [
       {
         components: [
@@ -80,6 +150,11 @@ const ABI = [
             name: "img",
             type: "string",
           },
+          {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+          },
         ],
         internalType: "struct donation.Dog[]",
         name: "",
@@ -89,11 +164,26 @@ const ABI = [
     stateMutability: "view",
     type: "function",
   },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
 ];
 let meta_connect;
 function App() {
   const [donate, setDonate] = useState(0);
+  const [donatetodog, setDonatetodog] = useState(0);
   const [dogs, setDogs] = useState([]);
+  const [balance, setbalance] = useState(0);
 
   async function mataconnect() {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -101,19 +191,33 @@ function App() {
     const singer = provider.getSigner();
     meta_connect = new ethers.Contract(ADDRESS, ABI, singer);
     console.log("Connected :", meta_connect);
-    getDogs();
+    getDog();
   }
 
-  async function getDogs() {
-    let res = await meta_connect.getDog();
+  async function getDog() {
+    let res = await meta_connect.getDogs();
+    let res2 = await meta_connect.balance();
     setDogs(res);
-    console.log(res);
+    setbalance(parseInt(res2["_hex"], 16));
+    console.log(res2);
   }
 
   async function doDonations() {
     try {
       let res = await meta_connect.doDonation({
         value: ethers.utils.parseEther(donate),
+      });
+      console.log(res);
+      alert("Donation Success !");
+    } catch (error) {
+      alert("Error : Pls check your token volume");
+    }
+  }
+
+  async function doDonationstoDog(id) {
+    try {
+      let res = await meta_connect.doDonatetoDog(id, {
+        value: ethers.utils.parseEther(donatetodog),
       });
       console.log(res);
       alert("Donation Success !");
@@ -130,6 +234,7 @@ function App() {
       <center>
         <h1>Dog Donation</h1>
         <br />
+        <h2>balance : {balance}</h2>
         <div
           style={{
             display: "flex",
@@ -153,7 +258,23 @@ function App() {
               <h3> name : {items[1]} </h3>
               <h4> city : {items[2]} </h4>
               <h5> note : {items[3]} </h5>
+              <h5> donate amount : {parseInt(items[5]["_hex"], 16)} </h5>
               <img src={items[4]} height="150" />
+              <br />
+              <input
+                onChange={(e) => {
+                  setDonatetodog(e.target.value);
+                }}
+              ></input>
+              <br />
+              <button
+                onClick={() => {
+                  let id = parseInt(items[0]["_hex"], 16);
+                  doDonationstoDog(id);
+                }}
+              >
+                Donate to this dog
+              </button>
             </div>
           ))}
         </div>
@@ -161,10 +282,10 @@ function App() {
         <br />
         <button
           onClick={() => {
-            getDogs();
+            getDog();
           }}
         >
-          getDogs
+          refresh data
         </button>
         <br />
         <input
@@ -177,7 +298,7 @@ function App() {
             doDonations();
           }}
         >
-          doDonation
+          Donate to web
         </button>
         <br />
       </center>
